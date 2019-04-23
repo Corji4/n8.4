4c25c42acc57b75901b953db4534eba7dcd3a6bf
Corji4 <beingevilhasaprice>
2019-04-24 00:59:17 +0300

    final
    программа соответствует всем условиям

--- main.cpp
+++ main.cpp
@@ -2,10 +2,29 @@
 
 int main()
 {
-	List<char> one;
-	one.add('g');
-	one.add('d');
-	one.print();
+	setlocale(LC_ALL, "RUS");
+	list<char> one, two, result;
+	string str;
+	cout << "¬ведите первый список:\n";
+	cin >> str;
+	for (int i = 0; i < str.size(); i++)
+	{
+		one.add(str.at(i));
+	}
+	cout << "¬ведите второй список:\n";
+	cin >> str;
+	for (int i = 0; i < str.size(); i++)
+	{
+		two.add(str.at(i));
+	}
+	for (int i = 0; i < one.get_size(); i++)
+	{
+		if (!result.check(one.at(i)) && !two.check(one.at(i)))
+		{
+			result.add(one.at(i));
+		}
+	}
+	result.print();
 	system("pause");
 	return 0;
 }
